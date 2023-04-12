af872411ca6e77e0c5b723de3fd84c9c86a77bb1
juhyeon-cha <28646684+juhyeon-cha@users.noreply.github.com>
2023-04-12 22:19:33 +0900

    3.6 패치 이후 지하맵 위치 보정

--- extension.js
+++ extension.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         게임닷 원신 맵스 확장
 // @namespace    view underground map
-// @version      2.1
+// @version      2.2
 // @description  원신 맵스에 여러 기능을 추가하는 유저스크립트
 // @author       juhyeon-cha
 // @match        https://genshin.gamedot.org/?mid=genshinmaps
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=gamedot.org
-// @updatelog    2023/04/12 v2.1 핀 그룹화 오류 수정
+// @updatelog    2023/04/12 v2.2 3.6 패치 이후 지하맵 위치 보정
 // @homepageURL  https://github.com/juhyeon-cha/genshin-maps-extension/
 // @downloadURL  https://github.com/juhyeon-cha/genshin-maps-extension/raw/main/extension.js
 // @updateURL    https://github.com/juhyeon-cha/genshin-maps-extension/raw/main/extension.js
@@ -17,259 +17,553 @@
 // @grant        GM_addStyle
 // ==/UserScript==
 
-const UNDERGROUND_IMAGES = [
+let UNDERGROUND_IMAGES = [
     {
-        'name': '간다르바_성곽_북_0',
-        'url': 'https://i.imgur.com/IAWkgXA.png',
-        'size': [700, 778, 38],
-        'offset': [6037, 7673]
+        "name": "간다르바_성곽_북_0",
+        "url": "https://i.imgur.com/IAWkgXA.png",
+        "size": [
+            700,
+            778,
+            38
+        ],
+        "offset": [
+            -3423,
+            2566
+        ]
     },
     {
-        'name': '간다언덕_동_0',
-        'url': 'https://i.imgur.com/oYuceah.png',
-        'size': [700, 1080, 32],
-        'offset': [6067, 7242]
+        "name": "간다언덕_동_0",
+        "url": "https://i.imgur.com/oYuceah.png",
+        "size": [
+            700,
+            1080,
+            32
+        ],
+        "offset": [
+            -3393,
+            2135
+        ]
     },
     {
-        'name': '과거의_바나라나',
-        'url': 'https://i.imgur.com/OSf2gnh.png',
-        'size': [700, 552, 74],
-        'offset': [4438, 7847]
+        "name": "과거의_바나라나",
+        "url": "https://i.imgur.com/OSf2gnh.png",
+        "size": [
+            700,
+            552,
+            74
+        ],
+        "offset": [
+            -5022,
+            2740
+        ]
     },
     {
-        'name': '과거의_바나라나_북_0',
-        'url': 'https://i.imgur.com/mVAg3fY.png',
-        'size': [800, 1149, 32],
-        'offset': [4398, 7041]
+        "name": "과거의_바나라나_북_0",
+        "url": "https://i.imgur.com/mVAg3fY.png",
+        "size": [
+            800,
+            1149,
+            32
+        ],
+        "offset": [
+            -5062,
+            1934
+        ]
     },
     {
-        'name': '과거의_바나라나_북_1',
-        'url': 'https://i.imgur.com/1FghM9T.png',
-        'size': [700, 871, 26],
-        'offset': [4440, 7374]
+        "name": "과거의_바나라나_북_1",
+        "url": "https://i.imgur.com/1FghM9T.png",
+        "size": [
+            700,
+            871,
+            26
+        ],
+        "offset": [
+            -5020,
+            2267
+        ]
     },
     {
-        'name': '다흐리의_폐허_0',
-        'url': 'https://i.imgur.com/IhyqBG5.png',
-        'size': [800, 396, 47],
-        'offset': [4769, 8992]
+        "name": "다흐리의_폐허_0",
+        "url": "https://i.imgur.com/IhyqBG5.png",
+        "size": [
+            800,
+            396,
+            47
+        ],
+        "offset": [
+            -4691,
+            3885
+        ]
     },
     {
-        'name': '데반타카산_남동_0',
-        'url': 'https://i.imgur.com/TnRoFcb.png',
-        'size': [700, 640, 52],
-        'offset': [6158, 8732]
+        "name": "데반타카산_남동_0",
+        "url": "https://i.imgur.com/TnRoFcb.png",
+        "size": [
+            700,
+            640,
+            52
+        ],
+        "offset": [
+            -3302,
+            3625
+        ]
     },
     {
-        'name': '데반타카산_남서_0',
-        'url': 'https://i.imgur.com/BOSKeFK.png',
-        'size': [800, 665, 46],
-        'offset': [5673, 8679]
+        "name": "데반타카산_남서_0",
+        "url": "https://i.imgur.com/BOSKeFK.png",
+        "size": [
+            800,
+            665,
+            46
+        ],
+        "offset": [
+            -3787,
+            3572
+        ]
     },
     {
-        'name': '데반타카산_동_0',
-        'url': 'https://i.imgur.com/ehQGwq3.png',
-        'size': [700, 503, 25],
-        'offset': [6433, 8502]
+        "name": "데반타카산_동_0",
+        "url": "https://i.imgur.com/ehQGwq3.png",
+        "size": [
+            700,
+            503,
+            25
+        ],
+        "offset": [
+            -3027,
+            3395
+        ]
     },
     {
-        'name': '데반타카산_북동_0',
-        'url': 'https://i.imgur.com/YluvQYF.png',
-        'size': [600, 692, 49],
-        'offset': [6232, 8277]
+        "name": "데반타카산_북동_0",
+        "url": "https://i.imgur.com/YluvQYF.png",
+        "size": [
+            600,
+            692,
+            49
+        ],
+        "offset": [
+            -3228,
+            3170
+        ]
     },
     {
-        'name': '마우티이마_숲_남동_0',
-        'url': 'https://i.imgur.com/GsOR3Uv.png',
-        'size': [700, 739, 38],
-        'offset': [6200, 6928]
+        "name": "마우티이마_숲_남동_0",
+        "url": "https://i.imgur.com/GsOR3Uv.png",
+        "size": [
+            700,
+            739,
+            38
+        ],
+        "offset": [
+            -3260,
+            1821
+        ]
     },
     {
-        'name': '마우티이마_숲_남동_1',
-        'url': 'https://i.imgur.com/L4I3MJU.png',
-        'size': [296, 200, 42],
-        'offset': [6444, 7422]
+        "name": "마우티이마_숲_남동_1",
+        "url": "https://i.imgur.com/L4I3MJU.png",
+        "size": [
+            296,
+            200,
+            42
+        ],
+        "offset": [
+            -3016,
+            2315
+        ]
     },
     {
-        'name': '마우티이마_숲_북동_0',
-        'url': 'https://i.imgur.com/TZNzERN.png',
-        'size': [700, 576, 52],
-        'offset': [5870, 6625]
+        "name": "마우티이마_숲_북동_0",
+        "url": "https://i.imgur.com/TZNzERN.png",
+        "size": [
+            700,
+            576,
+            52
+        ],
+        "offset": [
+            -3590,
+            1518
+        ]
     },
     {
-        'name': '마우티이마_숲_서_0',
-        'url': 'https://i.imgur.com/d85lP4L.png',
-        'size': [700, 964, 30],
-        'offset': [5885, 6738]
+        "name": "마우티이마_숲_서_0",
+        "url": "https://i.imgur.com/d85lP4L.png",
+        "size": [
+            700,
+            964,
+            30
+        ],
+        "offset": [
+            -3575,
+            1631
+        ]
     },
     {
-        'name': '비마라_마을_남서_0',
-        'url': 'https://i.imgur.com/so8atDV.png',
-        'size': [700, 627, 45],
-        'offset': [5515, 8422]
+        "name": "비마라_마을_남서_0",
+        "url": "https://i.imgur.com/so8atDV.png",
+        "size": [
+            700,
+            627,
+            45
+        ],
+        "offset": [
+            -3945,
+            3315
+        ]
     },
     {
-        'name': '비마라_마을_동_0',
-        'url': 'https://i.imgur.com/3GRx15c.png',
-        'size': [700, 337, 40],
-        'offset': [5869, 8427]
+        "name": "비마라_마을_동_0",
+        "url": "https://i.imgur.com/3GRx15c.png",
+        "size": [
+            700,
+            337,
+            40
+        ],
+        "offset": [
+            -3591,
+            3320
+        ]
     },
     {
-        'name': '선나원_북_0',
-        'url': 'https://i.imgur.com/kvKcR5C.png',
-        'size': [802, 658, 41],
-        'offset': [5089, 7973]
+        "name": "선나원_북_0",
+        "url": "https://i.imgur.com/kvKcR5C.png",
+        "size": [
+            802,
+            658,
+            41
+        ],
+        "offset": [
+            -4371,
+            2866
+        ]
     },
     {
-        'name': '선나원_북_1',
-        'url': 'https://i.imgur.com/gB87N9O.png',
-        'size': [700, 558, 62],
-        'offset': [5060, 7912]
+        "name": "선나원_북_1",
+        "url": "https://i.imgur.com/gB87N9O.png",
+        "size": [
+            700,
+            558,
+            62
+        ],
+        "offset": [
+            -4400,
+            2805
+        ]
     },
     {
-        'name': '수천삼림_남_0',
-        'url': 'https://i.imgur.com/JdnQgjG.png',
-        'size': [700, 647, 40],
-        'offset': [5078, 8937]
+        "name": "수천삼림_남_0",
+        "url": "https://i.imgur.com/JdnQgjG.png",
+        "size": [
+            700,
+            647,
+            40
+        ],
+        "offset": [
+            -4382,
+            3830
+        ]
     },
     {
-        'name': '아란나라_동_0',
-        'url': 'https://i.imgur.com/BkbmTpH.png',
-        'size': [800, 687, 39],
-        'offset': [4983, 7371]
+        "name": "아란나라_동_0",
+        "url": "https://i.imgur.com/BkbmTpH.png",
+        "size": [
+            800,
+            687,
+            39
+        ],
+        "offset": [
+            -4477,
+            2264
+        ]
     },
     {
-        'name': '야스나_유경_남_0',
-        'url': 'https://i.imgur.com/ckZNZsL.png',
-        'size': [700, 507, 55],
-        'offset': [4629, 8360]
+        "name": "야스나_유경_남_0",
+        "url": "https://i.imgur.com/ckZNZsL.png",
+        "size": [
+            700,
+            507,
+            55
+        ],
+        "offset": [
+            -4831,
+            3253
+        ]
     },
     {
-        'name': '차트라캄_동굴_0',
-        'url': 'https://i.imgur.com/K9iZfPB.png',
-        'size': [700, 1081, 43],
-        'offset': [5434, 6830]
+        "name": "차트라캄_동굴_0",
+        "url": "https://i.imgur.com/K9iZfPB.png",
+        "size": [
+            700,
+            1081,
+            43
+        ],
+        "offset": [
+            -4026,
+            1723
+        ]
     },
     {
-        'name': '다르알시파_0',
-        'url': 'https://i.imgur.com/3qLdt4p.png',
-        'size': [700, 828, 34],
-        'offset': [4335, 9405]
+        "name": "다르알시파_0",
+        "url": "https://i.imgur.com/3qLdt4p.png",
+        "size": [
+            700,
+            828,
+            34
+        ],
+        "offset": [
+            -5125,
+            4298
+        ]
     },
     {
-        'name': '다마반드산_0',
-        'url': 'https://i.imgur.com/2WHSZFL.png',
-        'size': [800, 761, 52],
-        'offset': [3321, 7948]
+        "name": "다마반드산_0",
+        "url": "https://i.imgur.com/2WHSZFL.png",
+        "size": [
+            800,
+            761,
+            52
+        ],
+        "offset": [
+            -6139,
+            2841
+        ]
     },
     {
-        'name': '다섯_오아시스의_생존자_0',
-        'url': 'https://i.imgur.com/qD1zPv5.png',
-        'size': [700, 400, 71],
-        'offset': [3589, 7450]
+        "name": "다섯_오아시스의_생존자_0",
+        "url": "https://i.imgur.com/qD1zPv5.png",
+        "size": [
+            700,
+            400,
+            71
+        ],
+        "offset": [
+            -5871,
+            2343
+        ]
     },
     {
-        'name': '다흐리_계곡_서_0',
-        'url': 'https://i.imgur.com/OcLNox6.png',
-        'size': [700, 856, 42],
-        'offset': [3554, 10095]
+        "name": "다흐리_계곡_서_0",
+        "url": "https://i.imgur.com/OcLNox6.png",
+        "size": [
+            700,
+            856,
+            42
+        ],
+        "offset": [
+            -5906,
+            4988
+        ]
     },
     {
-        'name': '다흐리_계곡_서_1',
-        'url': 'https://i.imgur.com/28VTfpy.png',
-        'size': [700, 974, 42],
-        'offset': [3793, 10023]
+        "name": "다흐리_계곡_서_1",
+        "url": "https://i.imgur.com/28VTfpy.png",
+        "size": [
+            700,
+            974,
+            42
+        ],
+        "offset": [
+            -5667,
+            4916
+        ]
     },
     {
-        'name': '도피의_언덕_남서_0',
-        'url': 'https://i.imgur.com/0GbvGew.png',
-        'size': [2290, 840, 14],
-        'offset': [2117, 9974]
+        "name": "도피의_언덕_남서_0",
+        "url": "https://i.imgur.com/0GbvGew.png",
+        "size": [
+            2290,
+            840,
+            14
+        ],
+        "offset": [
+            -7343,
+            4867
+        ]
     },
     {
-        'name': '부러진_정강이_협곡_0',
-        'url': 'https://i.imgur.com/SESBHtN.png',
-        'size': [700, 626, 74],
-        'offset': [2485, 7944]
+        "name": "부러진_정강이_협곡_0",
+        "url": "https://i.imgur.com/SESBHtN.png",
+        "size": [
+            700,
+            626,
+            74
+        ],
+        "offset": [
+            -6975,
+            2837
+        ]
     },
     {
-        'name': '세_운하의_땅_북_0',
-        'url': 'https://i.imgur.com/iQKgK1j.png',
-        'size': [600, 621, 61],
-        'offset': [2784, 7347]
+        "name": "세_운하의_땅_북_0",
+        "url": "https://i.imgur.com/iQKgK1j.png",
+        "size": [
+            600,
+            621,
+            61
+        ],
+        "offset": [
+            -6676,
+            2240
+        ]
     },
     {
-        'name': '세_운하의_땅_0',
-        'url': 'https://i.imgur.com/LK56eHQ.png',
-        'size': [800, 637, 60],
-        'offset': [2881, 7510]
+        "name": "세_운하의_땅_0",
+        "url": "https://i.imgur.com/LK56eHQ.png",
+        "size": [
+            800,
+            637,
+            60
+        ],
+        "offset": [
+            -6579,
+            2403
+        ]
     },
     {
-        'name': '적왕의_무덤_0',
-        'url': 'https://i.imgur.com/4AoJPpC.png',
-        'size': [1514, 1579, 57],
-        'offset': [2354, 9001]
+        "name": "적왕의_무덤_0",
+        "url": "https://i.imgur.com/4AoJPpC.png",
+        "size": [
+            1514,
+            1579,
+            57
+        ],
+        "offset": [
+            -7106,
+            3894
+        ]
     },
     {
-        'name': '적왕의_무덤_1',
-        'url': 'https://i.imgur.com/bbXIMpW.png',
-        'size': [700, 1162, 41],
-        'offset': [2796, 9333]
+        "name": "적왕의_무덤_1",
+        "url": "https://i.imgur.com/bbXIMpW.png",
+        "size": [
+            700,
+            1162,
+            41
+        ],
+        "offset": [
+            -6664,
+            4226
+        ]
     },
     {
-        'name': '적왕의_무덤_서_0',
-        'url': 'https://i.imgur.com/ZrDV2JC.png',
-        'size': [600, 1398, 42],
-        'offset': [2452, 8904]
+        "name": "적왕의_무덤_서_0",
+        "url": "https://i.imgur.com/ZrDV2JC.png",
+        "size": [
+            600,
+            1398,
+            42
+        ],
+        "offset": [
+            -7008,
+            3797
+        ]
     },
     {
-        'name': '신이_버린_신전_0',
-        'url': 'https://i.imgur.com/LQqO1Cy.png',
-        'size': [700, 727, 70],
-        'offset': [2671, 8808]
+        "name": "신이_버린_신전_0",
+        "url": "https://i.imgur.com/LQqO1Cy.png",
+        "size": [
+            700,
+            727,
+            70
+        ],
+        "offset": [
+            -6789,
+            3701
+        ]
     },
     {
-        'name': '신이_버린_신전_북_0',
-        'url': 'https://i.imgur.com/MmVuqWP.png',
-        'size': [700, 550, 70],
-        'offset': [2820, 8358]
+        "name": "신이_버린_신전_북_0",
+        "url": "https://i.imgur.com/MmVuqWP.png",
+        "size": [
+            700,
+            550,
+            70
+        ],
+        "offset": [
+            -6640,
+            3251
+        ]
     },
     {
-        'name': '신이_버린_신전_북_1',
-        'url': 'https://i.imgur.com/wbttkgz.png',
-        'size': [500, 377, 32],
-        'offset': [3076, 8653]
+        "name": "신이_버린_신전_북_1",
+        "url": "https://i.imgur.com/wbttkgz.png",
+        "size": [
+            500,
+            377,
+            32
+        ],
+        "offset": [
+            -6384,
+            3546
+        ]
     },
     {
-        'name': '알_아지프의_모래_0',
-        'url': 'https://i.imgur.com/qLHMu2T.png',
-        'size': [700, 760, 76],
-        'offset': [3760, 7927]
+        "name": "알_아지프의_모래_0",
+        "url": "https://i.imgur.com/qLHMu2T.png",
+        "size": [
+            700,
+            760,
+            76
+        ],
+        "offset": [
+            -5700,
+            2820
+        ]
     },
     {
-        'name': '영원의_오아시스_0',
-        'url': 'https://i.imgur.com/lH0bAk5.png',
-        'size': [700, 942, 48],
-        'offset': [3081, 7727]
+        "name": "영원의_오아시스_0",
+        "url": "https://i.imgur.com/lH0bAk5.png",
+        "size": [
+            700,
+            942,
+            48
+        ],
+        "offset": [
+            -6379,
+            2620
+        ]
     },
     {
-        'name': '자갈의_언덕_0',
-        'url': 'https://i.imgur.com/FTOn4CI.png',
-        'size': [700, 718, 66],
-        'offset': [2609, 9701]
+        "name": "자갈의_언덕_0",
+        "url": "https://i.imgur.com/FTOn4CI.png",
+        "size": [
+            700,
+            718,
+            66
+        ],
+        "offset": [
+            -6851,
+            4594
+        ]
     },
     {
-        'name': '자갈의_언덕_1',
-        'url': 'https://i.imgur.com/5BiYsbf.png',
-        'size': [700, 1075, 29],
-        'offset': [2402, 9663]
+        "name": "자갈의_언덕_1",
+        "url": "https://i.imgur.com/5BiYsbf.png",
+        "size": [
+            700,
+            1075,
+            29
+        ],
+        "offset": [
+            -7058,
+            4556
+        ]
     },
     {
-        'name': '희생_함정_0',
-        'url': 'https://i.imgur.com/dJhrFfI.png',
-        'size': [800, 732, 73],
-        'offset': [3871, 9349]
-    },
+        "name": "희생_함정_0",
+        "url": "https://i.imgur.com/dJhrFfI.png",
+        "size": [
+            800,
+            732,
+            73
+        ],
+        "offset": [
+            -5589,
+            4242
+        ]
+    }
 ];
 
 let IS_UNDERGROUND_ACTIVE = false;
@@ -351,9 +645,11 @@ function addUndergroundLayer() {
     const undergroundLayer = template.content.childNodes[1];
 
     UNDERGROUND_IMAGES.forEach((image, index) => {
+        const x = image.offset[0] + MAPS_RelativeX;
+        const y = image.offset[1] + MAPS_RelativeY;
         var template = document.createElement('template');
         template.innerHTML = `
-        <div class="underground-image" data-index="${index}" data-name="${image.name}" style="width: ${image.size[0]}px; height: ${image.size[1]}px; transform: translate(${image.offset[0]}px, ${image.offset[1]}px) scale(1);">
+        <div class="underground-image" data-index="${index}" data-name="${image.name}" style="width: ${image.size[0]}px; height: ${image.size[1]}px; transform: translate(${x}px, ${y}px) scale(1);">
             <div style="background-image: url(${image.url}); background-size: ${image.size[2]}%"></div>
         </div>`;
 
